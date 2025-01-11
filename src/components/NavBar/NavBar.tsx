69e65766925467790e78dfbcdd1b7b85ce5ceb4c
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2025-01-11 23:34:54 +0200

    Done making the navbar template working on fixing css and Lib file

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -8,11 +8,11 @@ const NavBar: React.FC = () => {
         <h2 className="text-3xl font-bold">
           Web <span className="text-emerald-600">Dev</span>
         </h2>
-        <div className="flex gap-x3">
+        <div className="flex gap-x-3">
           {LINKS.map((link) => {
             const { id, href, text } = link;
             return (
-              <a key={id} href={href}>
+              <a key={id} href={href} className='capitalize text-lg tracking-wide hover:text-emerald-600 duration-300'>
                 {text}
               </a>
             );
