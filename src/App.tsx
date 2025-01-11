4dfb74206b069fbb5cf8bd3038e9224251c16069
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2025-01-11 23:10:06 +0200

    Working on project navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react"
+import NavBar from "./components/NavBar/NavBar"
 
 
 const App: React.FC = () => {
 
   return (
     <>
-      <nav className="bg-emerald-100">Hello Tailwind</nav>
+      <NavBar/>
     </>
   )
 }
