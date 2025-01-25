5ceb98cebbc98e544b21a70c2e05ba18ad6d708c
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2025-01-25 19:11:17 +0200

    proceeding in the portfolcio structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.tsx'
-import './config/i18n/i18.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
