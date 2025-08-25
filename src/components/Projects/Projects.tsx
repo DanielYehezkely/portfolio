10d688762ba23c3857ac0143a9e75beb085b203d
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2025-08-25 17:20:47 +0300

    Added carousel package and build it for the projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,22 +2,45 @@ import React from 'react';
 
 import SectionTitle from '../SectionTitle/SectionTitle';
 import ProjectsCard from './ProjectsCard/ProjectsCard';
-
 import { PROJECTS } from '../../constants/data';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Projects: React.FC = () => {
   return (
-  <section className='py-20 align-element' id='projects'>
-    <SectionTitle text='web creations'/>
-    <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
-      {
-        PROJECTS.map((project) => {
-          return <ProjectsCard key={project.id} {...project}/>
-        })
-      }
-    </div>
-  </section>
-  )
+    <section className="py-20 align-element" id="projects">
+      <SectionTitle text="web creations" />
+
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay, A11y]}
+        className="py-16"
+        spaceBetween={24}
+        slidesPerView={1}
+        breakpoints={{
+          768: { slidesPerView: 2 }, 
+          1280: { slidesPerView: 3 }, 
+        }}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 3500,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false,
+        }}
+        loop={false}
+      >
+        {PROJECTS.map((project) => (
+          <SwiperSlide key={project.id}>
+            <ProjectsCard {...project} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Projects;
