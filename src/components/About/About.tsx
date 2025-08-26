ad85ca2dc9e2b370efd333f3d342c237ebefd064
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2025-08-26 13:16:09 +0300

    Done with the sections titles and content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
+import { useTranslation } from "react-i18next";
 
 const About: React.FC = () => {
+
+  const { t } = useTranslation('common');
+
   return (
     <section className="bg-white py-20 " id="about">
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
@@ -12,14 +16,7 @@ const About: React.FC = () => {
         <article className="">
           <SectionTitle i18nKey="sections.aboutTitle" />
           <p className="text-slate-600 mt-8 leading-loose">
-            I started coding at the end of 2023 and fell for the rhythm of
-            building: small ideas, quick iterations, sharper results. Then life
-            called—reserve duty—twice. Each pause pulled me from the keyboard,
-            but not from the craft. I kept notes, read docs on my phone, and
-            sketched features I’d ship “when I’m back.” I finished a front-end
-            course in between, served again until August 2025, and returned with
-            more grit than before. Now I’m back — ready to learn fast,
-            ship clean, and turn ideas into real products.
+           {t("sections.aboutBody")}
           </p>
         </article>
       </div>
