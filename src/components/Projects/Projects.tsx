331559aa0bf58fd628a2a24b338a35c0e015534a
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2025-08-26 13:23:19 +0300

    Done with fixing the carousel bug with the rtl

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,43 +1,46 @@
 import React from 'react';
-
 import SectionTitle from '../SectionTitle/SectionTitle';
 import ProjectsCard from './ProjectsCard/ProjectsCard';
 import { PROJECTS } from '../../constants/data';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay, A11y } from 'swiper/modules';
 import 'swiper/css';
 
-
 const Projects: React.FC = () => {
   return (
     <section className="py-20 align-element" id="projects">
-      <SectionTitle i18nKey='sections.projectsTitle' />
+      <SectionTitle i18nKey="sections.projectsTitle" />
 
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay, A11y]}
-        className="py-16"
-        spaceBetween={24}
-        slidesPerView={1}
-        breakpoints={{
-          768: { slidesPerView: 2 }, 
-          1280: { slidesPerView: 3 }, 
-        }}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 3500,
-          pauseOnMouseEnter: true,
-          disableOnInteraction: false,
-        }}
-        loop={false}
-      >
-        {PROJECTS.map((project) => (
-          <SwiperSlide key={project.id}>
-            <ProjectsCard {...project} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="relative py-16">
+        <Swiper
+          
+          dir="ltr"
+          key="ltr" 
+          modules={[Navigation, Pagination, Autoplay, A11y]}
+          spaceBetween={24}
+          slidesPerView={1}
+          breakpoints={{
+            768: { slidesPerView: 2 },
+            1280: { slidesPerView: 3 },
+          }}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 3500,
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+          }}
+          loop={false}
+          observer
+          observeParents
+        >
+          {PROJECTS.map((p) => (
+            <SwiperSlide key={p.id}>
+              <ProjectsCard {...p} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
