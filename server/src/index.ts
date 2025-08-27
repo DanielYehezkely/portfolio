0c22ce84c7692a367e38a2e917989fe325fa4e3a
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2025-08-27 17:27:32 +0300

    adding frontend build to the back for merging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import projectsRouter from './projects.routes';
@@ -14,4 +15,10 @@ app.use('/api/projects', projectsRouter);
 const port = Number(process.env.PORT) || 4000;
 app.listen(port, () => {
   console.log(`API running on http://localhost:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, '../dist-client')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../dist-client/index.html'));
 });
